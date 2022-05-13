b965563398beb92ac1b334cc46d5b95dd13dc052
Sovan <sovan.dey1@vodafone.com>
2022-05-13 15:07:14 +0530

    library

---
+++ projects/layout/src/lib/layout.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-layout',
+  templateUrl: 'lib-layout.html'
+})
+export class LayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
